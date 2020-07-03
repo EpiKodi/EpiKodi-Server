db8095b44af0e07b071082c486013b00e24a9ac0
Kylian <kylian.cadet@epitech.eu>
2020-07-04 01:56:40 +0200

    add database package + config 	modified:   Procfile 	modified:   app.py 	new file:   manage.py 	new file:   models.py 	modified:   requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
+from models import db
+import os
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db.init_app(app)
 
 @app.route("/")
 def hello():
