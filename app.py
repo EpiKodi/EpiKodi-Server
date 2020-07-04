9464cd9953ea46134ef852de339d33f436690f85
Kylian <kylian.cadet@epitech.eu>
2020-07-04 03:14:58 +0200

    add Docker integration 	modified:   .gitignore 	new file:   Dockerfile 	modified:   app.py 	new file:   docker-compose.yml 	new file:   launch.py 	modified:   models.py 	modified:   requirements.txt

--- app.py
+++ app.py
@@ -4,17 +4,14 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
 
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return name
-
-
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
