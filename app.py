117322b5f510e65c2e6693e37348d3d8b48ea703
Kylian <kylian.cadet@epitech.eu>
2020-07-04 17:28:52 +0200

    add flask-restful package add user model add register endpoint, with user creation 	modified:   app.py 	modified:   docker-compose.yml 	renamed:    migrations/versions/f582880ba619_.py -> migrations/versions/d219184cc243_.py 	modified:   models.py 	modified:   requirements.txt 	new file:   ressources/auth/register.py

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask
+from flask_restful import Resource, Api
+from ressources.auth.register import Register
 from models import db
 import os
 
+# Flask config
 app = Flask(__name__)
+app.config['DEBUG'] = os.environ.get('DEBUG')
+
+# Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
+# API config
+api = Api(app)
+api.add_resource(Register, '/auth/register')
+api.init_app(app)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
