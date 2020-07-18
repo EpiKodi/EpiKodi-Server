8a026ea2cc3ffc430f6249ca480d91aab4b68a33
Kylian <kylian.cadet@epitech.eu>
2020-07-18 21:43:29 +0200

    revert changes 	modified:   common/ws.py

--- common/ws.py
+++ common/ws.py
@@ -7,8 +7,7 @@ def emit_friends(user, message: str, obj: object) -> None:
     print("gonna emit to all friends")
     for friend in user.friends:
         print("gonna emit to : ", friend.username)
-        # emit(message, obj, room=friend.username, namespace='/')
-        emit(message, obj, namespace='/')
+        emit(message, obj, room=friend.username, namespace='/')
 
 
 def emit_user(user, message: str, obj: object) -> None:
